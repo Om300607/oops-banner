3629b58c558e0ff36428354474b172c744f2bc07
OM <30raiom@gmail.com>
2026-02-17 09:27:53 +0530

    Use case 2 Print the oops problem

--- op1.java
+++
@@ -1,6 +0,0 @@
-class op1
-{
-    public static void main(String[] args) {
-        System.out.println("OOPSss");
-    }
-}
---
+++ op2.java
@@ -0,0 +1,13 @@
+class op2
+{
+    public static void main(String Args[])
+        {
+            System.out.println("   ***       ***      ******      *****   ");
+            System.out.println("  **  **    **  **    **   **    **   **  ");
+            System.out.println(" **    **  **    **   **   **    **       ");
+            System.out.println(" **    **  **    **   ******      *****   ");
+            System.out.println(" **    **  **    **   **              **  ");
+            System.out.println("  **  **    **  **    **         **   **  ");
+            System.out.println("   ***       ***      **          *****   ");
+        }
+}
